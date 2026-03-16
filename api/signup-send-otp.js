dbf1c7231e30d6ba95d5f7238d08965211b9a072
Meghana N J <meghanamegha096@gmail.com>
2026-03-16 13:09:29 +0530

    first commit

--- api/signup-send-otp.js
+++ api/signup-send-otp.js
@@ -3,7 +3,7 @@ const { sql, initDB }           = require('./_db');
 const { generateOTP, sendOTP }  = require('./_mailer');
 const { saveOTP, getOTP }       = require('./_otpStore');
 
-module.exports = async (req, res) => {
+module.expsorts = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
